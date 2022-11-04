8c59f9863e78efed30f157c9c1ca6c035266791e
KoraKu <costhug@orange.fr>
2022-11-04 15:04:15 +0100

    removed old makefile (moved to project.h)

--- headers/const.h
+++ headers/const.h
@@ -15,51 +15,6 @@
 #include <errno.h>
 #include <sys/stat.h>
 
-#define MAKEFILE_C "CC := gcc\n\
-CC_FLAGS :=\n\
-\n\
-FILENAME := main.c\n\
-TARGET := ./bin/main\n\
-\n\
-SOURCES = $(wildcard src/*.c)\n\
-OBJECTS = $(patsubst src/%.c, obj/%.o, $(SOURCES))\n\
-\n\
-$(TARGET) : $(FILENAME) $(OBJECTS)\n\
-\t@echo \"Building $(FILENAME)...\"\n\
-\t$(CC) $(FILENAME) $(OBJECTS) $(CC_FLAGS) -o $(TARGET)\n\
-\n\
-obj/%.o : src/%.c\n\
-\t$(CC) -c $^ -o $@\n\
-\n\
-run : $(TARGET)\n\
-\t@echo \"Running $(TARGET) : \"\n\
-\t$(TARGET)\n\
-\n\
-clear :\n\
-\trm -f $(TARGET) $(OBJECTS)"
-
-#define MAKEFILE_CPP "CC := g++\n\
-CC_FLAGS :=\n\
-\n\
-FILENAME := main.cpp\n\
-TARGET := ./bin/main\n\
-\n\
-SOURCES = $(wildcard src/*.cpp)\n\
-OBJECTS = $(patsubst src/%.cpp, obj/%.o, $(SOURCES))\n\
-\n\
-$(TARGET) : $(FILENAME) $(OBJECTS)\n\
-\t@echo \"Building $(FILENAME)...\"\n\
-\t$(CC) $(FILENAME) $(OBJECTS) $(CC_FLAGS) -o $(TARGET)\n\
-\n\
-obj/%.o : src/%.cpp\n\
-\t$(CC) -c $^ -o $@\n\
-\n\
-run : $(TARGET)\n\
-\t@echo \"Running $(TARGET) : \"\n\
-\t$(TARGET)\n\
-\n\
-clear :\n\
-\trm -f $(TARGET) $(OBJECTS)"
 
 
 #define GITIGNORE "bin/\n!bin/info\ntodo/\n.o"
