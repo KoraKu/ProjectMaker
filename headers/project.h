222a34715006b71f1a33bb033858b16984787a23
KoraKu <costhug@orange.fr>
2022-11-04 15:10:30 +0100

    Updated the makefile #define's by adding \n

--- headers/project.h
+++ headers/project.h
@@ -5,70 +5,70 @@
 
 #include "const.h"
 
-#define MAKEFILE_C "COMPILER:= gcc\
-FLAGS := #add flags here. examples : -Wall\
-\
-FILENAME := main.c\
-TARGET := bin/main\
-\
-# Getting all sources files in src folder\
-SOURCES = $(wildcard src/*.c)\
-# Converting source files names to objects files\
-OBJECTS = $(patsubst src/%.c, obj/%.o, $(SOURCES))\
-\
-#Link main.c with every object\
-$(TARGET) : $(SOURCES) $(OBJECTS)\
-\tmkdir -p ./bin\
-\t$(COMPILER) $(FILENAME) $(wildcard obj/*.o) -o $(TARGET)\
-\
-#computes every object from sources\
-obj/%.o : src/%.c\
-\tmkdir -p ./obj\
-\t$(COMPILER) -c $^ -o $@\
-\
-#Clear eveything and rebuilds it\
-re : clear $(TARGET)\
-\
-#Runs the program\
-run : $(TARGET)\
-\t./$(TARGET)\
-\
-#Clear workspace of .o files and executable\
-clear :\
-\trm -f $(OBJECTS)\
+#define MAKEFILE_C "COMPILER:= gcc\n\
+FLAGS := #add flags here. examples : -Wall\n\
+\n\
+FILENAME := main.c\n\
+TARGET := bin/main\n\
+\n\
+# Getting all sources files in src folder\n\
+SOURCES = $(wildcard src/*.c)\n\
+# Converting source files names to objects files\n\
+OBJECTS = $(patsubst src/%.c, obj/%.o, $(SOURCES))\n\
+\n\
+#Link main.c with every object\n\
+$(TARGET) : $(SOURCES) $(OBJECTS)\n\
+\tmkdir -p ./bin\n\
+\t$(COMPILER) $(FILENAME) $(wildcard obj/*.o) -o $(TARGET)\n\
+\n\
+#computes every object from sources\n\
+obj/%.o : src/%.c\n\
+\tmkdir -p ./obj\n\
+\t$(COMPILER) -c $^ -o $@\n\
+\n\
+#Clear eveything and rebuilds it\n\
+re : clear $(TARGET)\n\
+\n\
+#Runs the program\n\
+run : $(TARGET)\n\
+\t./$(TARGET)\n\
+\n\
+#Clear workspace of .o files and executable\n\
+clear :\n\
+\trm -f $(OBJECTS)\n\
 \trm -f $(TARGET)"
 
-#define MAKEFILE_CPP "COMPILER:= g++\
-FLAGS := #add flags here. examples : -Wall\
-\
-FILENAME := main.cpp\
-TARGET := bin/main\
-\
-# Getting all sources files in src folder\
-SOURCES = $(wildcard src/*.cpp)\
-# Converting source files names to objects files\
-OBJECTS = $(patsubst src/%.cpp, obj/%.o, $(SOURCES))\
-\
-#Link main.c with every object\
-$(TARGET) : $(SOURCES) $(OBJECTS)\
-\tmkdir -p ./bin\
-\t$(COMPILER) $(FILENAME) $(wildcard obj/*.o) -o $(TARGET)\
-\
-#computes every object from sources\
-obj/%.o : src/%.cpp\
-\tmkdir -p ./obj\
-\t$(COMPILER) -c $^ -o $@\
-\
-#Clear eveything and rebuilds it\
-re : clear $(TARGET)\
-\
-#Runs the program\
-run : $(TARGET)\
-\t./$(TARGET)\
-\
-#Clear workspace of .o files and executable\
-clear :\
-\trm -f $(OBJECTS)\
+#define MAKEFILE_CPP "COMPILER:= g++\n\
+FLAGS := #add flags here. examples : -Wall\n\
+\n\
+FILENAME := main.cpp\n\
+TARGET := bin/main\n\
+\n\
+# Getting all sources files in src folder\n\
+SOURCES = $(wildcard src/*.cpp)\n\
+# Converting source files names to objects files\n\
+OBJECTS = $(patsubst src/%.cpp, obj/%.o, $(SOURCES))\n\
+\n\
+#Link main.c with every object\n\
+$(TARGET) : $(SOURCES) $(OBJECTS)\n\
+\tmkdir -p ./bin\n\
+\t$(COMPILER) $(FILENAME) $(wildcard obj/*.o) -o $(TARGET)\n\
+\n\
+#computes every object from sources\n\
+obj/%.o : src/%.cpp\n\
+\tmkdir -p ./obj\n\
+\t$(COMPILER) -c $^ -o $@\n\
+\n\
+#Clear eveything and rebuilds it\n\
+re : clear $(TARGET)\n\
+\n\
+#Runs the program\n\
+run : $(TARGET)\n\
+\t./$(TARGET)\n\
+\n\
+#Clear workspace of .o files and executable\n\
+clear :\n\
+\trm -f $(OBJECTS)\n\
 \trm -f $(TARGET)"
 
 int newpj(int argc, char *argv[], int language);
