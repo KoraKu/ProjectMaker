546ce7a92a76752ea1bbe13f5a15e3dc4b9776ad
KoraKu <costhug@orange.fr>
2022-11-04 17:37:23 +0100

    Now ignore the .pjm.info file in each project

--- headers/project.h
+++ headers/project.h
@@ -71,6 +71,6 @@ clear :\n\
 \trm -f $(OBJECTS)\n\
 \trm -f $(TARGET)"
 
-#define GITIGNORE "bin/\nobj/\ntodo/\ntest/"
+#define GITIGNORE "bin/\nobj/\ntodo/\ntest/\n.pjm.info"
 
 int newpj(int argc, char *argv[], int language);
